3ef771e1a3b75c08f4825dccd4351fc54ada646e
knjprojects <joshthereactdev@gmail.com>
2024-08-30 13:51:55 -0400

    starting ppwrite integration

--- src/components/daisyui/Form.tsx
+++ src/components/daisyui/Form.tsx
@@ -5,6 +5,8 @@ interface Props {}
 
 const Form = ({ sign }: { sign: string }) => {
   const [pass, setPass]: any = useState("password");
+  const [user, setUser]: any = useState("user");
+  const [email, setEmail]: any = useState("user@gmail.com");
   return (
     <div>
       {/*<label className="input input-bordered flex items-center gap-2">
@@ -32,7 +34,12 @@ const Form = ({ sign }: { sign: string }) => {
           <path d="M2.5 3A1.5 1.5 0 0 0 1 4.5v.793c.026.009.051.02.076.032L7.674 8.51c.206.1.446.1.652 0l6.598-3.185A.755.755 0 0 1 15 5.293V4.5A1.5 1.5 0 0 0 13.5 3h-11Z" />
           <path d="M15 6.954 8.978 9.86a2.25 2.25 0 0 1-1.956 0L1 6.954V11.5A1.5 1.5 0 0 0 2.5 13h11a1.5 1.5 0 0 0 1.5-1.5V6.954Z" />
         </svg>
-        <input type="text" className="grow" placeholder="Email" />
+        <input
+          type="text"
+          className="grow"
+          placeholder="Email"
+          onChange={(e) => setEmail(e.target.value)}
+        />
       </label>
       <label className="input input-bordered flex items-center gap-2">
         <svg
@@ -43,7 +50,12 @@ const Form = ({ sign }: { sign: string }) => {
         >
           <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6ZM12.735 14c.618 0 1.093-.561.872-1.139a6.002 6.002 0 0 0-11.215 0c-.22.578.254 1.139.872 1.139h9.47Z" />
         </svg>
-        <input type="text" className="grow" placeholder="Username" />
+        <input
+          type="text"
+          className="grow"
+          placeholder="Username"
+          onChange={(e) => setUser(e.target.value)}
+        />
       </label>
       <label className="input input-bordered flex items-center gap-2">
         <svg
--- src/lib/appwrite/api.ts
+++ src/lib/appwrite/api.ts
@@ -1,3 +1,16 @@
+import { account } from "./config";
 import { INewUser } from "./types";
-
-export async function createUserAccount(user: INewUser) {}
+import { ID } from "appwrite";
+export async function createUserAccount(user: INewUser) {
+  try {
+    const newaccount = await account.create(
+      ID.unique(),
+      user.email,
+      user.password,
+      user.name
+    );
+    return newaccount;
+  } catch (error) {
+    return error;
+  }
+}
